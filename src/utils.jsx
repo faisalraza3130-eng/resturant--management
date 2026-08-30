1ec555625062a69ded62f9cc8406780874ca2739
faisalraza3130-eng <faisalraza3130@gmail.com>
2026-08-31 01:03:29 +0500

    Updated currency format to [Amount] Rs

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const money = n => new Intl.NumberFormat('en-PK', {style:'currency', currency:'PKR'}).format(n);
+export const money = n => new Intl.NumberFormat('en-PK', { minimumFractionDigits: 2 }).format(n) + " Rs";
 
 export const badge = status => (
   <span className={`badge ${status.toLowerCase()}`}>{status}</span>
