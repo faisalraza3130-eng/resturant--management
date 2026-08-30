de478124e0d1f898f8ddafba5e2fcf259e844cd3
faisalraza3130-eng <faisalraza3130@gmail.com>
2026-08-31 01:05:18 +0500

    Removed decimals from currency format, e.g., 1400 Rs

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const money = n => new Intl.NumberFormat('en-PK', { minimumFractionDigits: 2 }).format(n) + " Rs";
+export const money = n => new Intl.NumberFormat('en-PK', { maximumFractionDigits: 0 }).format(Math.round(n)) + " Rs";
 
 export const badge = status => (
   <span className={`badge ${status.toLowerCase()}`}>{status}</span>
